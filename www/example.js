589c0d32b80d222fa693ba0c7b9315c107afaf12
Poul Sørensen <poulmobil+github@gmail.com>
2013-06-22 16:36:53 +0200

    c

--- www/example.js
+++ www/example.js
@@ -2,6 +2,6 @@ var http = require('http');
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello World\n');
-    console.log('hello world');
+   // console.log('hello world');
 }).listen(80, '10.0.0.4');
 console.log('Server running at http://s-innovations-unix.cloudapp.net:80/');
