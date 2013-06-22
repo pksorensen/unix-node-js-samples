1b36d85f062f36424c01a79186b254142a061a2f
Poul Sørensen <poul@s-innovations.dk>
2013-06-23 00:25:01 +0200

    port

--- HelloWorld/app.js
+++ HelloWorld/app.js
@@ -1,8 +1,9 @@
 var http = require('http');
 
+
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello World\n');
-}).listen(80, '10.0.0.4');
+}).listen(80);
 console.log('Server running at http://127.0.0.1:80/');
 
--- HelloWorld/app.ts
+++ HelloWorld/app.ts
@@ -1,9 +1,16 @@
 /// <reference path="./Scripts/typings/node/node.d.ts" />
+/// <reference path="./Scripts/typings/node-azure/azure.d.ts" />
 
-import http = module('http')
+import http = module('http');
+import azure = module('azure');
 
 http.createServer(function (req, res) {
+
+    
+
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello World\n');
-}).listen(80, '10.0.0.4');
+
+    
+}).listen(80);
 console.log('Server running at http://127.0.0.1:80/');
